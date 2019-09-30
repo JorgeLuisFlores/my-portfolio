11fc84afe01611d71d0f7bc7107700361e7b0b86
Jorge Flores <george130562@yahoo.com>
2019-09-30 16:25:27 -0700

    Commit

--- src/cardsimg.js
+++ src/cardsimg.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 const style = {
-  width: "15%",
-  height: "15%",
+  width: "25%",
+  height: "25%",
   marginTop: "2%",
   marginLeft: "1%",
   borderRadius: "10%"
@@ -14,23 +14,23 @@ const CardsIMG = props => {
     <div>
       <Card>
         <div>
-          <CardImg style={style} src={require("./img/pic.JPEG")} />
-        </div>
+          <CardImg style={style} src={require("./img/pic.JPEG")} />{" "}
+        </div>{" "}
         <CardBody>
           <CardTitle>
-            <b>About Me</b>
-          </CardTitle>
+            <b> About Me </b>{" "}
+          </CardTitle>{" "}
           <CardText>
-            I've worked in Operations Managment for over 10 years. <br></br>I'm
-            switching careers into tech because it's something I've always loved
-            but never had the opportunity to chase until now.<br></br> A few of
-            my hobbies/likes include video games, MTG, food, football and music.{" "}
-            <br></br> <br></br>
-            “Wisdom is not a product of schooling but of the lifelong attempt to
-            acquire it.” <br></br>― Albert Einstein
-          </CardText>
-        </CardBody>
-      </Card>
+            I 've worked in Operations Managment for over 10 years. <br></br>I'
+            m switching careers into tech because it 's something I' ve always
+            loved but never had the opportunity to chase until now. <br> </br> A
+            few of my hobbies / likes include video games, MTG, food, football
+            and music. <br> </br> <br></br> “Wisdom is not a product of
+            schooling but of the lifelong attempt to acquire it.” <br> </br>―
+            Albert Einstein{" "}
+          </CardText>{" "}
+        </CardBody>{" "}
+      </Card>{" "}
     </div>
   );
 };
